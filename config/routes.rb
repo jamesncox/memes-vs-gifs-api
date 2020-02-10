5391f5cbecfaf33e40c39546e14a2aff365814a4
jamesncox <jamesncox@yahoo.com>
2020-02-10 08:49:05 -0600

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-
-      get 'sessions/new'
-      get 'sessions/create'
-      get 'sessions/destroy'
-      resources :users
-
-    end
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
