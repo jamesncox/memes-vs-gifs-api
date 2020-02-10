51d06e5247937102bf058fda8f99f6dc631143e9
jamesncox <jamesncox@yahoo.com>
2020-02-10 08:59:24 -0600

    Update resources namespacing api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :users do
+
+      end
+
+    end
+  end
+
+  get "/auth" => 'sessions#auth'
+  get "/signup" => "accounts#new", as: "signup"
+  post "/signup" => "accounts#create"
+  get "/login" => "sessions#new", as: "login"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
 end
