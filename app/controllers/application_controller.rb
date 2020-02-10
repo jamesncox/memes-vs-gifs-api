da77d29cb1b5e3e579a1627d74a99101527ddab2
jamesncox <jamesncox@yahoo.com>
2020-02-10 09:04:58 -0600

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::API
     protect_from_forgery with: :exception
     before_action :set_csrf_cookie
 
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
     private
 
         def set_csrf_cookie
