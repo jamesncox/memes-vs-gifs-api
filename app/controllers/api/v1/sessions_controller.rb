32a1d43a11dbe8e659980c22a2bac3fee94ddd15
jamesncox <jamesncox@yahoo.com>
2020-02-10 09:07:19 -0600

    Update sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,10 +9,18 @@ class SessionsController < ApplicationController
   end
   
   def create
-
+    user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            log_in(user)
+            render json: user, status: 200
+        else
+            render json: { message: "Login credentials were incorrect, please try again.", error: true }
+        end
   end
 
   def destroy
-
+     # reset_session
+     session.delete(:user_id)
+     render json: {status: 200}
   end
 end
