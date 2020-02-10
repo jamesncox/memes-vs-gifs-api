79cc71f0b9c6d221e923b02af4ba5218e9e71e6c
jamesncox <jamesncox@yahoo.com>
2020-02-10 08:38:58 -0600

    Update namespacing to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+
+      get 'sessions/new'
+      get 'sessions/create'
+      get 'sessions/destroy'
+      resources :users
+
+    end
+  end
 end
