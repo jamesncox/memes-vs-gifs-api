47515606154068e3ad6e63c891d27d85115f70f1
jamesncox <jamesncox@yahoo.com>
2020-02-15 07:37:46 -0600

    Update gif and meme controllers

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,2 +1,22 @@
 class GifsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def gifs_params
+        end
+        
 end
--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,2 +1,21 @@
 class MemesController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def memes_params
+        end
 end
