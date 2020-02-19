b2eb061780f5a65d3bd431999cf32a9d43bc7046
jamesncox <jamesncox@yahoo.com>
2020-02-19 11:22:56 -0600

    Change port and import seed data

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,22 +1,34 @@
 class GifsController < ApplicationController
 
     def index
+        @gifs = Gif.all 
+        render json: @gifs, include: :caption, status: 200
     end
 
     def show
+        @gif = Gif.find_by(:id params[:id])
+        render json: @gif, include: :caption, status: 200
     end
 
     def create
+        @gif = Gif.create(gif_params)
+        render json: @gif, include: :caption, status: 200
     end
 
     def update
+        @gif = Gif.find_by(id: params[:id])
+        @gif.update(gif_params)
+        render json: @gif, include: :caption, status: 200
     end
 
     def destroy
+        @gif.destroy
+        head :no_content
     end
 
     private
-        def gifs_params
+        def gif_params
+            params.require(:gif).permit(:gif_id, :url)
         end
         
 end
--- app/controllers/api/v1/memes_controller.rb
+++ app/controllers/api/v1/memes_controller.rb
@@ -1,21 +1,33 @@
 class MemesController < ApplicationController
 
     def index
+        @memes = Meme.all 
+        render json: @memes, include: :caption, status: 200
     end
 
     def show
+        @meme = Meme.find_by(id: params[:id])
+        render json: @meme, include: :caption, status: 200
     end
 
     def create
+        @meme = Meme.create(meme_params)
+        render json: @meme, include: :caption, status: 200
     end
 
     def update
+        @meme = Meme.find_by(id: params[:id])
+        @meme.update(meme_params)
+        render json: @meme, include: :caption, status: 200
     end
 
     def destroy
+        @meme.destroy
+        head :no_content
     end
 
     private
-        def memes_params
+        def meme_params
+            params.require(:meme).permit(:meme_id, :url)
         end
 end
