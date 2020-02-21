b7fafe302f348d3b0014fdfb4285074c74c3589c
jamesncox <jamesncox@yahoo.com>
2020-02-21 15:37:39 -0600

    Refactor associations Gif and Meme

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,4 +1,5 @@
 class Meme < ApplicationRecord
     belongs_to :user
-    has_many :captions, as: :commentable
+    has_many :caption_captionables
+    has_many :captions, through: :caption_captionables
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3002'
+    origins 'http://localhost:3000', 'http://localhost:3001'
+
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # credentials: true
   end
 end
 
