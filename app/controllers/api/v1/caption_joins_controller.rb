63d9e7c4bdf587b6323534db2021c9237f6a2156
jamesncox <jamesncox@yahoo.com>
2020-02-25 08:59:53 -0600

    Comment out CSRF token methods, refactor CaptionJoins create action

--- app/controllers/api/v1/caption_joins_controller.rb
+++ app/controllers/api/v1/caption_joins_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::CaptionJoinsController < ApplicationController
+    # skip_before_action :verify_authenticity_token
+
+    def index
+        @caption_joins = CaptionJoin.all
+        render json: @caption_joins, status: 200
+    end
+
     def create
-        byebug
+        # byebug
         caption = Caption.find(params[:caption_id])
+        byebug
                 if params[:meme_id]
                     captionable = Meme.find(params[:meme_id])
                 else
@@ -17,6 +25,6 @@ class Api::V1::CaptionJoinsController < ApplicationController
 
     private
         def caption_join_params
-            params.require(:caption_join).permit(:caption_id, :captionable_id, :captionable_type)
+            params.require(:caption_join).permit(:caption_id, :captionable_id, :captionable_type, :meme_id, :gif_id)
         end
 end
