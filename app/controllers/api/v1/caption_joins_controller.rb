be48a3f4c4b7984bf64ffb233bc88c1340fb8024
jamesncox <jamesncox@yahoo.com>
2020-02-25 19:29:18 -0600

    Debug CaptionJoinsController, working create action

--- app/controllers/api/v1/caption_joins_controller.rb
+++ app/controllers/api/v1/caption_joins_controller.rb
@@ -7,20 +7,29 @@ class Api::V1::CaptionJoinsController < ApplicationController
     end
 
     def create
-        caption = Caption.find(params[:caption_id])
-        byebug
-            if params[:meme_id]
-                captionable = Meme.find(params[:meme_id])
-            else
-                captionable = Gif.find(params[:gif_id])
-            end
 
-        caption_join = CaptionJoin.find_or_create_by(caption: caption, captionable: captionable)
-            if caption_join.save 
-                render json: {error: false}
-            else
-                render json: {error: true}
-            end
+        # m = Meme.create(meme_id: "606060606", meme_url: "https://i.imgflip.com/ybuzd.jpg")
+        # c = Caption.find(45)
+        # CaptionJoins.create(caption: c, captionable: m)
+
+        meme = Meme.find_or_create_by(meme_id: params[:meme_id], meme_url: params[:meme_url])
+        caption = Caption.find_by(id: params[:caption_id])
+        CaptionJoin.create(caption: caption, captionable: meme)
+        render json: { message: "success" }
+
+        # meme = Meme.create(meme_id: params[:meme_id])
+        #     if params[:meme_id]
+        #         captionable = Meme.find(params[:meme_id])
+        #     else
+        #         captionable = Gif.find(params[:gif_id])
+        #     end
+
+        # caption_join = CaptionJoin.find_or_create_by(caption: caption, captionable: captionable)
+        #     if caption_join.save 
+        #         render json: {error: false}
+        #     else
+        #         render json: {error: true}
+        #     end
     end
 
     private
