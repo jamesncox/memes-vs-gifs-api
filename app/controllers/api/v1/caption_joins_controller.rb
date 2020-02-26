2437ece0a5ff054f7f04e30ba99094df650b1f09
jamesncox <jamesncox@yahoo.com>
2020-02-25 21:16:05 -0600

    Update CaptionJoins to save Gif with Caption

--- app/controllers/api/v1/caption_joins_controller.rb
+++ app/controllers/api/v1/caption_joins_controller.rb
@@ -7,14 +7,14 @@ class Api::V1::CaptionJoinsController < ApplicationController
     end
 
     def create
-        meme = Meme.find_or_create_by(meme_id: params[:meme_id], meme_url: params[:meme_url])
-        gif = Gif.find_or_create_by(gif_id: params[:gif_id], gif_url: params[:gif_url])
         caption = Caption.find_by(id: params[:caption_id])
 
-        if meme 
+        if params[:meme_id]
+            meme = Meme.find_or_create_by(meme_id: params[:meme_id], meme_url: params[:meme_url])
             CaptionJoin.create(caption: caption, captionable: meme)
             render json: { message: "success" }
         else
+            gif = Gif.find_or_create_by(gif_id: params[:gif_id], gif_url: params[:gif_url])
             CaptionJoin.create(caption: caption, captionable: gif)
             render json: { message: "success" }
         end
@@ -24,8 +24,6 @@ class Api::V1::CaptionJoinsController < ApplicationController
         def caption_join_params
             params.require(:caption_join).permit(
                 :caption_id, 
-                :captionable_id, 
-                :captionable_type, 
                 :text, :meme_id, 
                 :meme_url, 
                 :gif_id, 
