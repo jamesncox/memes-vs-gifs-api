51fb007d392869c2f445cca1823a85e88e857878
jamesncox <jamesncox@yahoo.com>
2020-02-25 20:06:51 -0600

    Refactor CaptionJoins controller to include gif conditional

--- app/controllers/api/v1/caption_joins_controller.rb
+++ app/controllers/api/v1/caption_joins_controller.rb
@@ -7,35 +7,29 @@ class Api::V1::CaptionJoinsController < ApplicationController
     end
 
     def create
-
-        # m = Meme.create(meme_id: "606060606", meme_url: "https://i.imgflip.com/ybuzd.jpg")
-        # c = Caption.find(45)
-        # CaptionJoins.create(caption: c, captionable: m)
-
         meme = Meme.find_or_create_by(meme_id: params[:meme_id], meme_url: params[:meme_url])
+        gif = Gif.find_or_create_by(gif_id: params[:gif_id], gif_url: params[:gif_url])
         caption = Caption.find_by(id: params[:caption_id])
-        CaptionJoin.create(caption: caption, captionable: meme)
-        render json: { message: "success" }
 
-        # meme = Meme.create(meme_id: params[:meme_id])
-        #     if params[:meme_id]
-        #         captionable = Meme.find(params[:meme_id])
-        #     else
-        #         captionable = Gif.find(params[:gif_id])
-        #     end
-
-        # caption_join = CaptionJoin.find_or_create_by(caption: caption, captionable: captionable)
-        #     if caption_join.save 
-        #         render json: {error: false}
-        #     else
-        #         render json: {error: true}
-        #     end
+        if meme 
+            CaptionJoin.create(caption: caption, captionable: meme)
+            render json: { message: "success" }
+        else
+            CaptionJoin.create(caption: caption, captionable: gif)
+            render json: { message: "success" }
+        end
     end
 
     private
         def caption_join_params
-            # something is wrong with my params they way they are written, either in schema, or front end? Namespacing doesn't match?
-            # also I will have to change my gif :url and meme :url attributes to :gif_url and :meme_url
-            params.require(:caption_join).permit(:caption_id, :captionable_id, :captionable_type, :text, :meme_id, :meme_url, :gif_id, :gif_url)
+            params.require(:caption_join).permit(
+                :caption_id, 
+                :captionable_id, 
+                :captionable_type, 
+                :text, :meme_id, 
+                :meme_url, 
+                :gif_id, 
+                :gif_url
+                )
         end
 end
