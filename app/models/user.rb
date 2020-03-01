36851b11303025c71845652f7418bf5d40a50ab5
jamesncox <jamesncox@yahoo.com>
2020-03-01 08:23:36 -0600

    Update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
  
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
   validates :password_confirmation, presence: true, on: :create
 end
