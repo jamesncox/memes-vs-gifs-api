d92999409786988395dd6a63197780c5b6cf662d
jamesncox <jamesncox@yahoo.com>
2020-03-07 10:03:52 -0600

    Debug caption error validations

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -2,4 +2,13 @@ class Caption < ApplicationRecord
     belongs_to :user, optional: true
     has_many :caption_joins
     has_many :captionables, through: :caption_joins
+
+    validates :text, presence: true, uniqueness: true
+    # validate :caption_rating
+
+    # def caption_rating
+    #     if rating != "PG" || "R"
+    #       errors.add(:caption, "You must select a PG or R rating")
+    #     end
+    # end
 end
