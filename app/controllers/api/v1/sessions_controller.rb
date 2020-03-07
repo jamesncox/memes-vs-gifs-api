a563812a5ac1f5f280f1dfadda1ff093b45df8fd
jamesncox <jamesncox@yahoo.com>
2020-03-06 23:11:32 -0600

    Debug user current_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,12 +5,14 @@ class Api::V1::SessionsController < ApplicationController
   end
   
   def create
+    # byebug
+    # something in here isn't sending the errors back correctly for logging in, because the user has everything correct, becuase it is created, so errors must come from something else.
     @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             render json: @user, status: 200
         else
-            render json: { errors: @user.errors.full_messages }, status: 400
+            render json: { errors: session.errors.full_messages }, status: 400
         end
   end
 
