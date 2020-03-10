5c3da5d8e09594157be1b9c3a784c2007e123928
jamesncox <jamesncox@yahoo.com>
2020-03-10 12:05:06 -0500

    Add user_id, caption_text and caption_id to gifs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_165710) do
+ActiveRecord::Schema.define(version: 2020_03_10_170336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2020_03_10_165710) do
     t.string "gif_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.text "caption_text"
+    t.integer "caption_id"
   end
 
   create_table "memes", force: :cascade do |t|
