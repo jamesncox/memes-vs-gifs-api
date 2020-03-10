9c999910cb4e8af93a2367c1c2714e85b038bd11
jamesncox <jamesncox@yahoo.com>
2020-03-10 13:04:33 -0500

    test commit Meme.rb

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,5 +1,6 @@
 class Meme < ApplicationRecord
-    belongs_to :user
+    #test commit
+    
     has_many :caption_joins, as: :captionable
     has_many :captions, through: :caption_joins, as: :captionable
 end
