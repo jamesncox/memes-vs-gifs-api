1d3a62d416004136e345e10a710a5520bcea5fdf
jamesncox <jamesncox@yahoo.com>
2020-06-08 11:58:13 -0500

    Update Users Controller private user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
+        def user_params
+            params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        end
 end
